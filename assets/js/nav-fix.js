bc14dab9336822ac43068c9d7f32019b612ce019
Abhinav MS <abhinavms@outlook.com>
2020-04-26 16:18:31 +0530

    Fixed Navbar section-link

--- assets/js/nav-fix.js
+++ assets/js/nav-fix.js
@@ -1,4 +1,10 @@
 $(document).ready(function () {
+    
+    $('body').scrollspy({
+      target: '#myNavbar',
+      offset: 50
+    });
+    
     $(window).scroll(function () {
         if ($(document).scrollTop() > 100) {
             $("nav").addClass("scrolled");
